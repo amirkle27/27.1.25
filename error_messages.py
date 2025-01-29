b8864368d3cda1582ff574331324f826c8f0e833
amirkle27 <amirkle@gmail.com>
2025-01-29 11:52:56 +0200

    Create error_messages.py

---
+++ error_messages.py
@@ -0,0 +1,27 @@
+class NameNotStrError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+class EmptyNameError(Exception):
+    def __init__(self,error):
+        super().__init__(error)
+        
+class ImproperEmailError(Exception):
+    def __init__(self,error):
+        super().__init__(error)
+
+class BalanceNotFloatError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+class DepositBelowZeroError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+class NonActiveAccountError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
+
+class MaxNumberOfAccountsError(Exception):
+    def __init__(self, error):
+        super().__init__(error)
